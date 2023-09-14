e1b4f6858891551dfb50b8943342fbb5d82664c0
Varlotte <32116877+Varlotte@users.noreply.github.com>
2023-09-14 11:25:48 -0400

    passes 4 tests

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_list = []
+    num1 = 0
+    num2= 1
+    next_number= 0
+
+    for n in range(length):
+        fibonacci_list.append(next_number)
+        num1 = num2
+        num2 = next_number
+        next_number = num1 + num2
+        
+    print(fibonacci_list)
+    
